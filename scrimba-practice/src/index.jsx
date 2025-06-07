a7a987350720cbb7efbff6a205a035c9f13fed2b
kylemucha <kylemuchaswe@gmail.com>
2025-06-06 23:29:36 -0700

    fixes

--- scrimba-practice/src/index.jsx
+++ scrimba-practice/src/index.jsx
@@ -5,18 +5,6 @@ import MainContent from "./MainContent"
 import Footer from "./Footer"
 const root = createRoot(document.getElementById("root"))
 
-/**
- * Challenge:
- * 
- * Using flexbox, line up the `li`s horizontally and put them inline
- * with the React logo.
- * 
- * NOTE: for practice's sake, don't select any
- * elements, but use classes for all styling.
- */
-
-
-
 function OmmyFacts () {
     return (
         <>
