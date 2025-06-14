572fa1524ea88d5ca71a52c5a363fac470797cbe
kylemucha <kylemuchaswe@gmail.com>
2025-06-14 02:47:21 -0700

    progress

--- ReactFacts/src/components/Main.jsx
+++ ReactFacts/src/components/Main.jsx
@@ -1,5 +1,16 @@
 export default function Main () {
     return (
-        <h1>Main</h1>
+        <>
+        <div className="main-content">
+            <h1>Fun facts about React</h1>
+            <ul className="list-items">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on GitHub </li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+           </ul>
+        </div>
+        <img className="half-logo" src="/react-logo.png" alt="Half React Logo Faded in Background" />
+        </>
     )
 }
