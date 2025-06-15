01ac68d6bf0b2e611aa25e0c60697af0a43fedd7
kylemucha <kylemuchaswe@gmail.com>
2025-06-14 22:33:02 -0700

    Capstone Project: ReactFacts

--- ReactFacts/src/components/Main.jsx
+++ ReactFacts/src/components/Main.jsx
@@ -2,15 +2,16 @@ export default function Main () {
     return (
         <>
         <div className="main-content">
-            <h1>Fun facts about React</h1>
+            <h1 className="main-header">Fun facts about React</h1>
             <ul className="list-items">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 200K stars on GitHub </li>
+                <li>Is maintained by Meta</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
            </ul>
         </div>
-        <img className="half-logo" src="/react-logo.png" alt="Half React Logo Faded in Background" />
+        <img className="half-logo" src="/gray-react-logo.png" alt="Half React Logo Faded in Background" />
         </>
     )
 }
